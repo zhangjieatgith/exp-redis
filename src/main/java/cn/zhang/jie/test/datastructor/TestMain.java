64baa91c9642e520497ea1539a62e6043bc565ef
zhangjiee <zhangjieee@1.q>
2018-08-30 22:17:17 +0800

    有序集合测试

--- src/main/java/cn/zhang/jie/test/datastructor/TestMain.java
+++ src/main/java/cn/zhang/jie/test/datastructor/TestMain.java
@@ -2,14 +2,19 @@ package cn.zhang.jie.test.datastructor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.redis.connection.RedisListCommands;
-import org.springframework.data.redis.core.RedisCommand;
+import org.springframework.data.redis.connection.RedisZSetCommands.Limit;
+import org.springframework.data.redis.connection.RedisZSetCommands.Range;
+import org.springframework.data.redis.core.DefaultTypedTuple;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
 /**
  * 6中数据类型
@@ -21,9 +26,109 @@ public class TestMain {
 	public static void main(String[] args) {
 //		m1();
 //		m2();
-		m3();
+//		m3();
+		m4();
 	}
 	
+	
+	/**
+	 * 操作有序集合
+	 */
+	@SuppressWarnings({"unchecked","rawtypes"})
+	public static void m4() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		//这里也是用的字符串序列化器
+		RedisTemplate redisTemplate = context.getBean("hashRedisTemplate", RedisTemplate.class);
+		//TypedTuple 这是一个接口，它定义了两个 方法，getValue()获取值、getScore()获取分数
+		Set<TypedTuple> set1 = new HashSet<TypedTuple>();
+		Set<TypedTuple> set2 = new HashSet<TypedTuple>();
+		//
+		int j = 9;
+		for(int i=1;i<=9;i++) {
+			j--;
+			//计算分数和值
+			Double score1 = Double.valueOf(i);
+			String value1 = "x"+i;
+			Double score2 = Double.valueOf(j);
+			String value2 = j % 2 == 1 ? "y" + j : "x"+j;
+			//使用spring提供的默认TypedTuple -> DefaultTypedTuple
+			TypedTuple typedTuple1 = new DefaultTypedTuple(value1, score1);
+			set1.add(typedTuple1);
+			TypedTuple typedTuple2 = new DefaultTypedTuple(value2, score2);
+			set2.add(typedTuple2);
+		}
+		System.out.println("zset1 -------> "+set1);
+		System.out.println("zset2 -------> "+set2);
+		redisTemplate.opsForZSet().add("zset1", set1);
+		redisTemplate.opsForZSet().add("zset2", set2);
+		//统计总数
+		Long size = null;
+		size = redisTemplate.opsForZSet().zCard("zset1");
+		System.out.println(size);
+		//求数量：记分数为score，那么下面的方法就是求 3<=score<=6 的元素
+		size = redisTemplate.opsForZSet().count("zset1", 3, 6);
+		System.out.println(size);
+		//求集合：从下标一开始截取5个元素，但是不返回分数，每一个元素是string。由于分数的存在，这些值是有序的，因此可以向链表一样使用下标
+		Set set = null;
+		set = redisTemplate.opsForZSet().range("zset1", 1, 5);
+		System.out.println(set);
+		//截取集合所有元素，并且对集合按分数排序，并返回分数，每一个元素是TypedTuple。貌似range方法已经隐式的排序过了
+		set = redisTemplate.opsForZSet().rangeWithScores("zset1", 0, -1);
+		printTypedTuple(set);
+		//将zset1和zset2两个集合的交集存放到集合inter_zset中
+		size = redisTemplate.opsForZSet().intersectAndStore("zset1", "zset2", "inter_zset");
+		System.out.println(size);
+		//区间操作，感觉像，先取x8的索引，再取x2的索引，然后根据这两个索引，取出以这两个索引为端节点的集合
+		Range range = Range.range();
+		range.lt("x8");
+		range.gt("x2");
+		set = redisTemplate.opsForZSet().rangeByLex("zset1", range);
+		System.out.println(set);
+		range.lte("x8");
+		range.gte("x2");
+		set = redisTemplate.opsForZSet().rangeByLex("zset1", range);
+		System.out.println(set);
+		//限制返回个数
+		Limit limit = Limit.limit();
+		limit.count(4);
+		limit.offset(5);
+		//求区间内的元素，并限制返回4条
+		set = redisTemplate.opsForZSet().rangeByLex("zset1", range, limit);
+		System.out.println(set);	//这个结果是综合 limit 的count和offset得出来的
+		//求排行，排名第一返回0，第二返回1
+		Long rank = redisTemplate.opsForZSet().rank("zset1","x4");
+		System.out.println("rank = "+rank);
+		//删除元素，返回删除个数
+		size = redisTemplate.opsForZSet().remove("zset1", "x5","x6");
+		System.out.println(size);
+		//按照排行删除从0开始算起，这里将排行第二和第3的元素删除
+		size = redisTemplate.opsForZSet().removeRange("zset1", 1, 2);
+		System.out.println(size);
+		//获取集合中的所有元素的值和分数，-1表示全部元素
+		set = redisTemplate.opsForZSet().rangeWithScores("zset2", 0, -1);
+		printTypedTuple(set);
+		//删除指定的元素
+		size = redisTemplate.opsForZSet().remove("zset2", "y5","y3");
+		System.out.println(size);
+		//给集合中一个元素的score加上11,调试观察（以下三步似乎有些问题）
+		Double dbl = redisTemplate.opsForZSet().incrementScore("zset1", "x1", 11);
+		redisTemplate.opsForZSet().removeRangeByScore("zset1", 1, 2);
+		set = redisTemplate.opsForZSet().reverseRangeWithScores("zset2", 1, 10);
+		printTypedTuple(set);
+		context.close();
+	}
+	
+	@SuppressWarnings({"rawtypes"})
+	private static void printTypedTuple(Set<TypedTuple>set){
+		if(set != null && set.isEmpty()) {
+			return;
+		}
+		Iterator<TypedTuple> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			TypedTuple val = iterator.next();
+			System.out.println("{value = "+val.getValue()+",score = "+val.getScore()+"}\n");
+		}
+	}
 	
 	/**
 	 * 操作集合
